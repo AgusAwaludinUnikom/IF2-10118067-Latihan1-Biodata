ec23f3febac16aa24497fcd75e9b0dc82ad6f72a
Agus Awaludin <55693286+AgusAwaludinUnikom@users.noreply.github.com>
2019-09-23 18:17:03 +0700

    Add files via upload

---
+++ src/if2/pkg10118067/latihan1/IF210118067Latihan1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10118067.latihan1;
+
+/**
+ *
+ * @author
+ * Nama     : Agus Awaludin
+ * Kelas    : IF-2
+ * NIM      : 10118067
+ * Latihan1. Perintah Menampilkan Value Ke Layar
+ */
+public class IF210118067Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        {
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Agus Awaludin");
+        System.out.println("ALAMAT\t\t: Jalan Nagrog 3 No.33");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 25 Agustus 2000");
+        System.out.println("----------------------");
+        System.out.println("HOBI\t\t: Tidur");
+        System.out.println("MAKES\t\t: Cimol");
+        System.out.println("MIKES\t\t: Capucino");
+       
+    }
+    
+}
+
+}
